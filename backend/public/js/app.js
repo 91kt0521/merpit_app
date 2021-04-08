d065daf4754c547f4ff14113e73ff4bf00520a9f
K_T <armn7guc.supbs.3@gmail.com>
2021-04-08 21:17:13 +0900

    [update]:インデント修正

--- backend/public/js/app.js
+++ backend/public/js/app.js
@@ -1,17 +1,15 @@
 
-
+// 画像を選択するinputタグのDOMを取得
 document.querySelector('.image-picker input')
-
 .addEventListener('change', (e)=> {
     // ここに画像が選択された時の処理を記載する
 
-    // 画像を読みこむ方法
-
-    // e.target はクリックされた <li> 要素を参照します
+    // e.target はクリックされた要素を参照します
     const input = e.target;
     console.log(input);
-    const reader = new FileReader();
 
+    // 画像を読みこむ方法
+    const reader = new FileReader();
     reader.onload = (e) => {
         // ここに、画像を読み込んだ後の処理を記述する
         //読み込んだ画像をimgタグに表示する
