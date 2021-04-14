607c50613e834f647fdc4acb2e3a1cfab4112fa1
K_T <armn7guc.supbs.3@gmail.com>
2021-04-14 21:49:59 +0900

    [update]:migrateでデータ作成時にエラーになった原因のため元に戻す

--- backend/app/Models/PrimaryCategory.php
+++ backend/app/Models/PrimaryCategory.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class PrimaryCategory extends Model
 {
+    use HasFactory;
     // 以下の処理でリレーションを定義
     // $category->secondaryCategoriesで大カテゴリに紐づく小カテゴリの一覧を取得
     public function secondaryCategories() {
