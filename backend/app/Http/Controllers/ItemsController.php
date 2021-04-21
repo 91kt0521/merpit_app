ba4e7d98efcf3c594310fed30f2c4b6a867410ac
K_T <armn7guc.supbs.3@gmail.com>
2021-04-21 21:53:52 +0900

    [add]:検索機能の実装

--- backend/app/Http/Controllers/ItemsController.php
+++ backend/app/Http/Controllers/ItemsController.php
@@ -8,12 +8,48 @@ use Illuminate\Http\Request;
 class ItemsController extends Controller
 {
     public function showItems(Request $request) {
-        $items = Item::orderByRaw( "FIELD(state, '" . Item::STATE_SELLING . "', '" . Item::STATE_BOUGHT . "')" )
+
+        $query = Item::query();
+
+        // カテゴリで絞り込む
+        if ($request->filled('category')) {
+            // カテゴリ：1の形式を分解しそれぞれ変数に代入
+            list($categoryType, $categoryId) = explode(':', $request->input('category'));
+
+            // カテゴリ事に条件を指定
+            if ($categoryType == 'primary') {
+                // 大カテゴリのIDはitemテーブルに保持していないためリレーションに接続し小カテゴリテーブルから取得する
+                $query->whereHas('secondaryCategory', function($query) use ($categoryId){
+                    $query->where('primary_category_id', $categoryId);
+                });
+            } elseif($categoryType == 'secondary') {
+                $query->where('secondary_category_id', $categoryId);
+            }
+        }
+
+        // キーワードで絞り込み
+        if ($request->filled('keyword')) {
+            $keyword = '%' . $this->escape($request->input('keyword')) . '%';
+            $query->where(function ($query) use ($keyword) {
+                $query->where('name', 'LIKE', $keyword);
+                $query->orWhere('description', 'LIKE', $keyword);
+            });
+        }
+
+        $items = $query->orderByRaw( "FIELD(state, '" . Item::STATE_SELLING . "', '" . Item::STATE_BOUGHT . "')" )
             ->orderBy('id', 'DESC')
-            ->paginate(1);
+            ->paginate(10);
 
         return view('items.items')
             ->with('items', $items);
+    }
+
+    private function escape(string $value) {
+        return str_replace(
+            ['\\', '%', '_'],
+            ['\\\\', '\\%', '\\_'],
+            $value
+        );
     }
 
     public function showItemDetaile(Item $item) {
