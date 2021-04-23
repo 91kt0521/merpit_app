83e57e7ff2b23c55ad913c1ea29e635c5165a5e8
K_T <armn7guc.supbs.3@gmail.com>
2021-04-23 20:59:40 +0900

    [add]:検索入力値が検索後に保持されるように修正

--- backend/app/View/Components/Header.php
+++ backend/app/View/Components/Header.php
@@ -3,6 +3,7 @@
 namespace App\View\Components;
 
 use App\Models\PrimaryCategory;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\Component;
 
@@ -36,8 +37,15 @@ class Header extends Component
             ->orderBy('sort_no')
             ->get();
 
+        $defaults = [
+            'category' => Request::input('category'),
+            'keyword' => Request::input('keyword'),
+        ];
+        var_dump($defaults);
+
         return view('components.header')
             ->with('user', $user)
-            ->with('categories', $categories);
+            ->with('categories', $categories)
+            ->with('defaults', $defaults);
     }
 }
